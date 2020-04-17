2224080b9d8d61d0573b419226772b2032149a06
Eduardo-L-R <e.leiva.rocuant@gmail.com>
2020-04-17 16:34:55 -0400

    actualizando archivos viernes 17 de abril

--- src/App.js
+++ src/App.js
@@ -1,29 +1,72 @@
 import React from 'react';
 import './App.css';
 
-class App extends React.Component() {
+class App extends React.Component{
   constructor(){
     super();
     this.state ={
-      task:"",
+      info:{}
     }
+    this.imprimirValoresJson = this.imprimirValoresJson.bind(this);
+    this.recorrerCategoria = this.recorrerCategoria.bind(this);
   }
   componentDidMount(){
-    fetch('https://httpbin.org/ip')
+
+    this.asignarValores((data)=>{
+      let setInfo = this.state;
+      setInfo.info = data;
+      this.setState(setInfo);
+    });
+
+  }
+
+  asignarValores = (callback)=>{
+    fetch("https://omgvamp-hearthstone-v1.p.rapidapi.com/info",
+    {
+      method: 'GET',
+      headers: {
+        "x-rapidapi-host": "omgvamp-hearthstone-v1.p.rapidapi.com",
+        "x-rapidapi-key": "952bef1824mshbfe707ca173d53fp101874jsnb414afe463f0"
+      }})
     .then(function(response) {
-        return response.text();
+        return response.json();
     })
     .then(function(data) {
-        console.log('data = ', data);
+        callback(data);
     })
     .catch(function(err) {
         console.error(err);
     });
   }
 
+
+  imprimirValoresJson(){
+    let ValoresJson = this.state.info;
+    let array = [];
+    let contador = 0;
+    let arrayContenido = Object.values(this.state.info);
+    for (var key in ValoresJson){
+          array.push(key);
+    }
+    console.log("este es array: ",array);
+    console.log("este es arrayContenido: ",arrayContenido);
+    array.map(categoria =>{
+      console.log("Categoria : ",categoria)
+      let CategoriasArray = [];
+      CategoriasArray.push(categoria);
+      this.recorrerCategoria(arrayContenido,contador);
+      contador = contador +1;
+    });
+
+  }
+  recorrerCategoria(arrayContenido,contador){
+    if(contador !== 0){Object.values(arrayContenido[contador]).map(categoriaobjeto => console.log(categoriaobjeto))}
+    else{console.log(arrayContenido[contador])}
+  }
   render(){
     return(
       <div>
+        {this.imprimirValoresJson()} 
       </div>
     )
   };
